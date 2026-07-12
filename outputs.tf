00830f15015f22807fda222379093fec9d95a24c
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:45 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,45 +1,41 @@
-output "maintenance_configurations" {
-  description = "All maintenance_configuration resources"
-  value       = azurerm_maintenance_configuration.maintenance_configurations
-}
 output "maintenance_configurations_in_guest_user_patch_mode" {
-  description = "List of in_guest_user_patch_mode values across all maintenance_configurations"
-  value       = [for k, v in azurerm_maintenance_configuration.maintenance_configurations : v.in_guest_user_patch_mode]
+  description = "Map of in_guest_user_patch_mode values across all maintenance_configurations, keyed the same as var.maintenance_configurations"
+  value       = { for k, v in azurerm_maintenance_configuration.maintenance_configurations : k => v.in_guest_user_patch_mode }
 }
 output "maintenance_configurations_install_patches" {
-  description = "List of install_patches values across all maintenance_configurations"
-  value       = [for k, v in azurerm_maintenance_configuration.maintenance_configurations : v.install_patches]
+  description = "Map of install_patches values across all maintenance_configurations, keyed the same as var.maintenance_configurations"
+  value       = { for k, v in azurerm_maintenance_configuration.maintenance_configurations : k => v.install_patches }
 }
 output "maintenance_configurations_location" {
-  description = "List of location values across all maintenance_configurations"
-  value       = [for k, v in azurerm_maintenance_configuration.maintenance_configurations : v.location]
+  description = "Map of location values across all maintenance_configurations, keyed the same as var.maintenance_configurations"
+  value       = { for k, v in azurerm_maintenance_configuration.maintenance_configurations : k => v.location }
 }
 output "maintenance_configurations_name" {
-  description = "List of name values across all maintenance_configurations"
-  value       = [for k, v in azurerm_maintenance_configuration.maintenance_configurations : v.name]
+  description = "Map of name values across all maintenance_configurations, keyed the same as var.maintenance_configurations"
+  value       = { for k, v in azurerm_maintenance_configuration.maintenance_configurations : k => v.name }
 }
 output "maintenance_configurations_properties" {
-  description = "List of properties values across all maintenance_configurations"
-  value       = [for k, v in azurerm_maintenance_configuration.maintenance_configurations : v.properties]
+  description = "Map of properties values across all maintenance_configurations, keyed the same as var.maintenance_configurations"
+  value       = { for k, v in azurerm_maintenance_configuration.maintenance_configurations : k => v.properties }
 }
 output "maintenance_configurations_resource_group_name" {
-  description = "List of resource_group_name values across all maintenance_configurations"
-  value       = [for k, v in azurerm_maintenance_configuration.maintenance_configurations : v.resource_group_name]
+  description = "Map of resource_group_name values across all maintenance_configurations, keyed the same as var.maintenance_configurations"
+  value       = { for k, v in azurerm_maintenance_configuration.maintenance_configurations : k => v.resource_group_name }
 }
 output "maintenance_configurations_scope" {
-  description = "List of scope values across all maintenance_configurations"
-  value       = [for k, v in azurerm_maintenance_configuration.maintenance_configurations : v.scope]
+  description = "Map of scope values across all maintenance_configurations, keyed the same as var.maintenance_configurations"
+  value       = { for k, v in azurerm_maintenance_configuration.maintenance_configurations : k => v.scope }
 }
 output "maintenance_configurations_tags" {
-  description = "List of tags values across all maintenance_configurations"
-  value       = [for k, v in azurerm_maintenance_configuration.maintenance_configurations : v.tags]
+  description = "Map of tags values across all maintenance_configurations, keyed the same as var.maintenance_configurations"
+  value       = { for k, v in azurerm_maintenance_configuration.maintenance_configurations : k => v.tags }
 }
 output "maintenance_configurations_visibility" {
-  description = "List of visibility values across all maintenance_configurations"
-  value       = [for k, v in azurerm_maintenance_configuration.maintenance_configurations : v.visibility]
+  description = "Map of visibility values across all maintenance_configurations, keyed the same as var.maintenance_configurations"
+  value       = { for k, v in azurerm_maintenance_configuration.maintenance_configurations : k => v.visibility }
 }
 output "maintenance_configurations_window" {
-  description = "List of window values across all maintenance_configurations"
-  value       = [for k, v in azurerm_maintenance_configuration.maintenance_configurations : v.window]
+  description = "Map of window values across all maintenance_configurations, keyed the same as var.maintenance_configurations"
+  value       = { for k, v in azurerm_maintenance_configuration.maintenance_configurations : k => v.window }
 }
 
